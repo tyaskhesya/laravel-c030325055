583905d14fa828b8dfc48baeec2dbc5769f098ca
tyaskhesya <khesyatyas@gmail.com>
2026-09-22 23:52:47 +0800

    Perbarui README proyek Laravel

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,12 @@
+<h1>Daftar Mahasiswa</h1> 
+<table border="1" cellpadding="8">    
+    <tr><th>NIM</th><th>Nama</th><th>Prodi</th><th>Semester</th></tr>    
+    @foreach ($data as $mhs)   
+    <tr>        
+        <td>{{ $mhs->nim }}</td>        
+        <td>{{ $mhs->nama }}</td>        
+        <td>{{ $mhs->prodi }}</td>        
+        <td>{{ $mhs->semester }}</td>    
+    </tr>    
+    @endforeach 
+</table>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Mahasiswa;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/mahasiswa', function () {    
+    $data = Mahasiswa::all();    
+    return view('mahasiswa.index', compact('data'));
 });
